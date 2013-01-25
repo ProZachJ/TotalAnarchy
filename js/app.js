a5eac622aee35ddca900641f535295567d1c5c48
Zachary Jones <prozachJ@gmail.com>
2013-01-25 17:55:24 -0600

    Install Angular-Strap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Initialize Application
 var totalAnarchy = {};
-var App = angular.module('totalAnarchy',[]);
+var App = angular.module('totalAnarchy',['$strap.directives']);
 
 function loadCtrl($scope){
 	$scope.test = 'tester';
