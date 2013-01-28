d3a7f99a68f970f687b3fdd9a8614b6ae5480712
Zachary Jones <prozachJ@gmail.com>
2013-01-28 15:08:32 -0600

    More Cleanup

--- js/controllers/serverCtrl.js
+++ js/controllers/serverCtrl.js
@@ -1,5 +1,15 @@
 function serverCtrl($scope, server){
-	server.start();
+	$scope.serverstate = 'Not Listening';
 
+	$scope.listen = function (){
+		if ($scope.serverstate === 'Not Listening'){
+			server.start();
+			$scope.serverstate = 'Listening';
+		}
+		else if ($scope.serverstate === 'Listening'){
+			server.stop();
+			$scope.serverstate = 'Not Listening';
+		}
+	};
 	$scope.request = server.request();
 }
